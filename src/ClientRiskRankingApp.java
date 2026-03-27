2ec88ab9fda6fbab70e28eb0ac2d0291e057cb6f
YENIGALLADHEERAJ <dheerajyenigalla@gmail.com>
2026-03-27 15:18:58 +0530

    Problem 2: Client Risk Ranking implementation

---
+++ src/ClientRiskRankingApp.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Client {
+    String name;
+    int riskScore;
+    double accountBalance;
+
+    Client(String name, int riskScore, double accountBalance) {
+        this.name = name;
+        this.riskScore = riskScore;
+        this.accountBalance = accountBalance;
+    }
+
+    @Override
+    public String toString() {
+        return name + ":" + riskScore;
+    }
+}
+
+public class ClientRiskRankingApp {
+    public static void main(String[] args) {
+        Client[] clients = {
+                new Client("clientC", 80, 5000),
+                new Client("clientA", 20, 15000),
+                new Client("clientB", 50, 2000)
+        };
+
+        bubbleSort(clients);
+        System.out.println("Bubble (asc): " + Arrays.toString(clients));
+
+        insertionSort(clients);
+        System.out.println("Insertion (desc): " + Arrays.toString(clients));
+    }
+
+    public static void bubbleSort(Client[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].riskScore > arr[j + 1].riskScore) {
+                    Client temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(Client[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            Client key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].riskScore < key.riskScore) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
